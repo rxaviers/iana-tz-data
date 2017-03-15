14140a9e4c1b6292016ff1647c7efbc806f94af1
Rafael Xavier de Souza <rxaviers@gmail.com>
2017-03-15 15:41:48 -0300

    Include isdsts information

--- converter/index.js
+++ converter/index.js
@@ -5,11 +5,11 @@ var output = {
   zoneData: {}
 };
 
-momentTimezoneJson.zones.forEach(({name, abbrs, untils, offsets}) => {
+momentTimezoneJson.zones.forEach(({name, abbrs, untils, offsets, isdsts}) => {
   name = name.split('/');
   name.slice(0, -1).reduce(function (tree, item) {
     return tree[item] || (tree[item] = {});
-  }, output.zoneData)[name.slice(-1)[0]] = {abbrs, untils, offsets};
+  }, output.zoneData)[name.slice(-1)[0]] = {abbrs, untils, offsets, isdsts};
 })
 
 console.log(JSON.stringify(output, null, 2));
